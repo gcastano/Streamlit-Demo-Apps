e05d78485cbee2e7ee5120c9ac1595ec488800c1
Germán Andrés Castaño Vásquez <german@nagence.com>
2024-07-21 20:47:45 -0500

    Ajustes básicos de código a ejemplo de humanización de datos

--- streamlitHumanize.py
+++ streamlitHumanize.py
@@ -2,9 +2,8 @@ import streamlit as st
 import humanize # https://python-humanize.readthedocs.io/en/latest/
 from millify import millify # https://github.com/azaitsev/millify
 from datetime import date, datetime
-from streamlit_date_picker import date_picker, PickerType
+from streamlit_date_picker import date_picker, PickerType # https://github.com/imdreamer2018/streamlit-date-picker
 import locale
-
 
 
 hoy = date.today()
@@ -16,15 +15,27 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
+
 st.header('Ejemplo de humanización de valores y fechas')
+
 parIdioma = st.radio('Idioma',options=['Inglés','Español'],index=0,horizontal=True)
 idiomaValor = 'en'
+
 if parIdioma == 'Español':    
     _t = humanize.i18n.activate("es_ES") # Cambiamos a español para Humanize
     locale.setlocale(locale.LC_ALL,'es_ES.UTF-8') # Cambiamos a español para Python
     idiomaValor = 'es'
 
 def human_format(num,lang='en'):
+    """Convierte un número a un texto más fácil de interpretar por las personas
+
+    Args:
+        num (float): Número a convertir
+        lang (str, optional): Lenguaje usado para la conversión ya que los valores de billones en adelante es diferente en español que en inglés. Defaults to 'en' otro valor válido 'es'.
+
+    Returns:
+        str: Texto con el valor convertido a un texto legible
+    """    
     num = float('{:.3g}'.format(num))
     magnitude = 0
     while abs(num) >= 1000:
@@ -37,7 +48,10 @@ def human_format(num,lang='en'):
     else:        
         valueScale = ['', 'K', 'M', 'B', 'T']
     return '{} {}'.format('{:,.1f}'.format(num).rstrip('0').rstrip('.'),valueScale[magnitude])
+
+
 st.divider()
+
 st.subheader('Humanizando :blue[valores numéricos]')
 parValor = st.number_input('Número')
 
@@ -45,14 +59,14 @@ c1,c2,c3,c4=st.columns(4)
 with c1:
     st.subheader('Formatos nativos')
     st.metric('Valor Original',parValor)
-    st.metric('Valor Formato',f'{parValor:,.0f}')
+    st.metric('Valor Formato',f'{parValor:,.2f}')
 with c2:
     st.subheader('Formatos Humanize')
     st.metric('Valor intcomma',humanize.intcomma(parValor))
     st.metric('Valor intcomma decimales',humanize.intcomma(parValor,2))
     st.metric('Valor intword',humanize.intword(parValor))
 with c3:
-    st.subheader('Formatos Milify')
+    st.subheader('Formatos Millify')
     st.metric('Valor millify simple',millify(parValor))
     st.metric('Valor millify con decimales',millify(parValor,2))
 with c4:
@@ -97,5 +111,5 @@ with c2:
     st.subheader('Formatos Humanize')
     st.metric('Valor naturaltime',humanize.naturaltime(parFechaHora))
     st.metric('Valor precisedelta simple',humanize.precisedelta(parFechaHora,format='%0.2f'))
-    st.metric('Valor precisedelta sin segundos',humanize.precisedelta(parFechaHora,format='%0.2f',minimum_unit='minutes'))
+    st.metric('Valor precisedelta sin segundos',humanize.precisedelta(parFechaHora,format='%0.2f',minimum_unit='hours'))
     
